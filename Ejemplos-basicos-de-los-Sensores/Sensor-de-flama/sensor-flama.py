8526013af9c98ff3fefb229c0e27e97f84805d4a
José Ernesto Calvillo Lizárraga <106187704+JoErnesto@users.noreply.github.com>
2022-12-19 19:40:47 -0800

    Se agregarón comentarios.

--- Ejemplos-basicos-de-los-Sensores/Sensor-de-flama/sensor-flama.py
+++ Ejemplos-basicos-de-los-Sensores/Sensor-de-flama/sensor-flama.py
@@ -1,20 +1,27 @@
 #Sensor de flama
+#Se importa el modulo de control de la interfaz de GPIO.
 import RPi.GPIO as GPIO
+#Se importa la función sleep de la biblioteca time.
 from time import sleep
+#Se configura el pin 4 como entrada.
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(4, GPIO.IN)
 
 try:
     while True:
         if GPIO.input(4):
+            #Se imprime el valor leido del sensor.
             print(GPIO.input(4))
         
         else:
+            #Se imprime el valor leido del sensor.
             print(GPIO.input(4))
         
+        #Espera 1 segundo.
         sleep(1)
         
 finally:
+    #Limpia los puertos GPIO.
     print("Cleaning up..")
     GPIO.cleanup()
     
