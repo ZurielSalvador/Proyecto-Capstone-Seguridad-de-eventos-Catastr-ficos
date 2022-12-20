44bb8f53148413fc4d2886c8b06552d743da80e0
José Ernesto Calvillo Lizárraga <106187704+JoErnesto@users.noreply.github.com>
2022-12-19 20:04:05 -0800

    Se agregaron comentarios.

--- Proyecto-Seguridad.py
+++ Proyecto-Seguridad.py
@@ -1,6 +1,7 @@
 #Proyecto Seguridad de Eventos Catastróficos
 #Generic Reed dem inputs (GPIO4)
 
+#Se importan las bibliotecas para los puertos GPIO, MQTT, JSON, SLEEP, SMBUS y MLX60914.
 import RPi.GPIO as GPIO
 import paho.mqtt.client as mqtt
 import json
@@ -9,9 +10,11 @@ from time import sleep
 
 from smbus2 import SMBus
 from mlx90614 import MLX90614
+#Se selecciona el bus 1.
 bus = SMBus(1)
+#Se asigna en una variable el bus y el puerto de donde se conectó el sensor.
 sensor = MLX90614(bus, address=0x5A)
-
+#Se configura los pines GPIO 4 y 17 como entradas.
 GPIO.setmode(GPIO.BCM)
 GPIO.setup (4, GPIO.IN)
 GPIO.setup (17, GPIO.IN)
@@ -20,6 +23,7 @@ client = mqtt.Client()
 
 try:
     while True:
+        #Se realiza el JSON que contiene la información de los sensores y la identificación del hogar.
         datos = { #Modifición
     'casaID': 4,
     'temperatura':sensor.get_obj_temp(),
@@ -33,17 +37,20 @@ try:
         if GPIO.input(4):
             
             #print(GPIO.input(4),"Temperatura ambiente :", sensor.get_amb_temp())
-            
+            #Se conecta al broker utilizado.
             client.connect("3.124.62.105", 1883, 60)
+            #Publica en el tema el JSON creado anteriormente.
             client.publish("SeguridadCatastrofes/Departamento/cocina",json.dumps(datos))           
             
         else:
             
             #print(GPIO.input(4),"Temperatura ambiente :", sensor.get_amb_temp())
+            #Se conecta al broker utilizado.
             client.connect("3.124.62.105", 1883, 60)
+            #Publica en el tema el JSON creado anteriormente.
             client.publish("SeguridadCatastrofes/Departamento/cocina",json.dumps(datos))
                      
-            
+        #Espera 1 segundo
         sleep(1)
 
             
