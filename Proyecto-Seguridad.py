5748133b7e8a471ef25e388ebd9068e1b4bfea69
ZurielSalvador <106187746+ZurielSalvador@users.noreply.github.com>
2022-12-20 21:20:22 -0600

    Se agregan Comentarios

--- Proyecto-Seguridad.py
+++ Proyecto-Seguridad.py
@@ -1,6 +1,8 @@
 #Proyecto Seguridad de Eventos Catastróficos
 #Generic Reed dem inputs (GPIO4)
 
+
+#Se importan las bibliotecas para los puertos GPIO, MQTT, JSON, SLEEP, SMBUS y MLX60914.
 import RPi.GPIO as GPIO
 import paho.mqtt.client as mqtt
 import json
@@ -9,9 +11,14 @@ from time import sleep
 
 from smbus2 import SMBus
 from mlx90614 import MLX90614
+
+
 bus = SMBus(1)
+
+#Se asigna en una variable el bus y el puerto de donde se conectó el sensor.
 sensor = MLX90614(bus, address=0x5A)
 
+#Se configura los pines GPIO 4 y 17 como entradas.
 GPIO.setmode(GPIO.BCM)
 GPIO.setup (4, GPIO.IN)
 GPIO.setup (17, GPIO.IN)
@@ -28,6 +35,8 @@ client = mqtt.Client()
 
 try:
     while True:
+
+        #Se realiza el JSON que contiene la información de los sensores y la identificación del hogar.
         datos = { #Modifición
     'casaID': 1,
     'temperatura':sensor.get_obj_temp(),
@@ -43,7 +52,10 @@ try:
             #print(GPIO.input(4),"Temperatura ambiente :", sensor.get_amb_temp())
             #SeguridadCatastrofes/Departamento/cocina
             
+            #Se conecta al broker utilizado.
             client.connect("18.197.95.2", 1883, 60)
+
+            #Publica en el tema el JSON creado anteriormente.
             client.publish("SeguridadCatastrofes/Departamento/cocina",json.dumps(datos))
             GPIO.output(BUZZER_PIN, GPIO.LOW)
             sleep(1)
