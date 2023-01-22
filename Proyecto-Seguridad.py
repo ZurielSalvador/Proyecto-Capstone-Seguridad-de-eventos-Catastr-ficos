0a3c682b742d14d632038a2a61e998bb0360c78e
ZurielSalvador <106187746+ZurielSalvador@users.noreply.github.com>
2023-01-22 03:02:00 -0600

    Modifiación de comentarios.

--- Proyecto-Seguridad.py
+++ Proyecto-Seguridad.py
@@ -1,8 +1,12 @@
-#Proyecto Seguridad de Eventos Catastróficos
-#Generic Reed dem inputs (GPIO4)
+#Proyecto: Seguridad de Eventos Catastróficos
+#Autores:
+#Victor Zuriel Dominguez Salvador
+#Jose Ernesto Calvillo Lizárraga
+#Eduardo Cabrera Mendoza
 
 
-#Se importan las bibliotecas para los puertos GPIO, MQTT, JSON, SLEEP, SMBUS y MLX60914.
+
+#Se importan las bibliotecas GPIO, MQTT, JSON, SLEEP, SMBUS y MLX60914.
 import RPi.GPIO as GPIO
 import paho.mqtt.client as mqtt
 import json
@@ -15,10 +19,10 @@ from mlx90614 import MLX90614
 
 bus = SMBus(1)
 
-#Se asigna en una variable el bus y el puerto de donde se conectó el sensor.
+#Se asigna en una variable bus y el puerto de dónde se conectó el sensor infrarrojo .
 sensor = MLX90614(bus, address=0x5A)
 
-#Se configura los pines GPIO 4 y 17 como entradas.
+#Se configura los pines GPIO 4 y 17 como entradas para el sensor de flama y Gas.
 GPIO.setmode(GPIO.BCM)
 GPIO.setup (4, GPIO.IN)
 GPIO.setup (17, GPIO.IN)
@@ -26,7 +30,7 @@ GPIO.setup (17, GPIO.IN)
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 
-BUZZER_PIN = 5
+BUZZER_PIN = 5    #Se asigana el pin de salida 5 para el Buzzer.
 
 GPIO.setup(BUZZER_PIN, GPIO.OUT)
 
@@ -36,8 +40,8 @@ client = mqtt.Client()
 try:
     while True:
 
-        #Se realiza el JSON que contiene la información de los sensores y la identificación del hogar.
-        datos = { #Modifición
+        #Se realiza el JSON que contiene la información de los sensores y la  ubicacioón e identificación del hogar.
+        datos = { #Modificación
     'casaID': 1,
     'temperatura':sensor.get_obj_temp(),
     'gas': GPIO.input(17),
@@ -47,25 +51,26 @@ try:
     'direccion': 'Renato Leduc, Puente de Piedra, TLALPAN, Coyoacan, 14040 Ciudad de Mexico',
     
     }
-        if GPIO.input(17):
+        if GPIO.input(17): 
             
-            #print(GPIO.input(4),"Temperatura ambiente :", sensor.get_amb_temp())
-            #SeguridadCatastrofes/Departamento/cocina
             
-            #Se conecta al broker utilizado.
+            #Se conecta al broker utilizado con ayuda de mqtt.Client.
+            #El número 1883 es el puerto.
+            #El número 60 es cada cuanto se intenta reconectar.
             client.connect("18.197.95.2", 1883, 60)
 
-            #Publica en el tema el JSON creado anteriormente.
+            #Se Publica en el tema el JSON creado anteriormente.
+            #El tema a publicar es: SeguridadCatastrofes/Departamento/cocina
             client.publish("SeguridadCatastrofes/Departamento/cocina",json.dumps(datos))
-            GPIO.output(BUZZER_PIN, GPIO.LOW)
+            GPIO.output(BUZZER_PIN, GPIO.LOW) #Se apagará el buzzer si ya no detecta Gas.
             sleep(1)
             
         else:
             
-            #print(GPIO.input(4),"Temperatura ambiente :", sensor.get_amb_temp())
+            
             client.connect("18.197.95.2", 1883, 60)
             client.publish("SeguridadCatastrofes/Departamento/cocina",json.dumps(datos))
-            GPIO.output(BUZZER_PIN, GPIO.HIGH)
+            GPIO.output(BUZZER_PIN, GPIO.HIGH) #Se prenderá el Buzzer al detectar Gas constantemente.
             sleep(1)
                      
             
